ff9ce9f510344c3f16bbb72019934b38b9a2022b
nemo2109 <114691858+nemo2109@users.noreply.github.com>
2022-12-05 13:22:13 -0600

    Funcionamiento de JS creado

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const trabajo = document.getElementById("work");
 const telefono = document.getElementById("phone");
 const sobre = document.getElementById("about");
 
-
+// LLamar a la forma
 const form = document.getElementById("forma");
+
+// Const para el contenedor
+const contenedor = document.getElementById("contenedor");
 
 //Crear event listener para la forma 
 form.addEventListener('submit', (e) => {
@@ -14,13 +17,36 @@ form.addEventListener('submit', (e) => {
    e.preventDefault();
 
    //Crear objeto que tengas los valores de la forma
-   const formulario ={
+   const formulario = {
     name: nombre.value,
-    email: correo.value,
-    
+    gmail: correo.value,
+    job: trabajo.value,
+    cell: telefono.value,
+    about: sobre.value,
    }
+
+   const { name, gmail, job, cell, about } = formulario;
+
+   const cuadroHtmlConInfo = crearCuadroDeForma
+   (name, gmail, job, cell, about);
+
+   contenedor.insertAdjacentHTML('beforeend', cuadroHtmlConInfo);
 
    //Dar la orden de submit
    //event.submit();
 
 });
+
+// Crear una funcion que nos retorne contenido HTML dinamicamente
+function crearCuadroDeForma(name, gmail, job, cell, about){
+   const cuadroHtml = `
+   <div class="contenedor">
+        <h1 class="contenedor__nombre">${name}</h1>
+        <p class="contendor__p">${gmail}</p>
+        <p class="contenedor__p">${job}</p>
+        <p class="contenedor__p">${cell}</p>
+        <p class="contenedor__p">${about}</p>
+   </div>
+   `
+   return cuadroHtml
+}
